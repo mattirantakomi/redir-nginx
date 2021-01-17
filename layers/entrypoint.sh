eddc691815bce86d179a808ea5b354d3ac0a5d90
Matti Rantakömi <matti@rantakomi.fi>
2021-01-17 09:26:26 +0000

    lisätty kohteen ip

--- layers/entrypoint.sh
+++ layers/entrypoint.sh
@@ -19,8 +19,9 @@ done
 IFS=$OLDIFS
 
 REDIRECT_FROM_INTERFACE_IP="$(ifconfig ${REDIRECT_FROM_INTERFACE} | grep 'inet ' | awk '{ print $2 }')"
+SUBDOMAIN=$(grep search /etc/resolv.conf | awk '{ print $2 }')
+REDIRECT_TO_IP=$(dig ingress-nginx-controller.${SUBDOMAIN} +short)
 
-echo "Setting up iptables prerouting rule"
 echo "iptables -t nat -I PREROUTING 1 -i ${REDIRECT_FROM_INTERFACE} -p tcp -m tcp --dport ${REDIRECT_TO_PORT} -j ACCEPT"
 iptables -t nat -I PREROUTING 1 -i ${REDIRECT_FROM_INTERFACE} -p tcp -m tcp --dport ${REDIRECT_TO_PORT} -j ACCEPT
 echo
@@ -28,9 +29,23 @@ echo
 echo "Redirect from interface ${REDIRECT_FROM_INTERFACE} public IP is ${REDIRECT_FROM_INTERFACE_IP} on node ${NODE_HOSTNAME}"
 echo
 
-echo "Redirecting port ${REDIRECT_FROM_PORT} to ${REDIRECT_TO}:${REDIRECT_TO_PORT}"
+echo "Redirecting port ${REDIRECT_FROM_PORT} to ${REDIRECT_TO} (${REDIRECT_TO_IP}) port ${REDIRECT_TO_PORT}"
 /usr/bin/redir -s -n ":${REDIRECT_FROM_PORT}" "${REDIRECT_TO}:${REDIRECT_TO_PORT}" &
 echo
 
 pid=$!
 wait $!
+
+
+
+;; ANSWER SECTION:
+ingress-nginx-controller.ingress-nginx.svc.cluster.local. 5 IN A 10.43.24.7
+
+;; Query time: 1 msec
+;; SERVER: 10.43.0.10#53(10.43.0.10)
+;; WHEN: Sun Jan 17 09:24:11 UTC 2021
+;; MSG SIZE  rcvd: 169
+
+root@node3:/# dig ingress-nginx-controller.${SUBDOMAIN} +short
+10.43.24.7
+
