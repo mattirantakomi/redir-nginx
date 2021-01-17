9373b4f6483c186b5e32f3856cb936b5133420ca
Matti Rantakömi <matti@rantakomi.fi>
2021-01-17 09:08:01 +0000

    fiksailut jatkuu

--- layers/entrypoint.sh
+++ layers/entrypoint.sh
@@ -18,8 +18,12 @@ for RULE in $(iptables-save | grep PREROUTING | grep "${REDIRECT_FROM_INTERFACE}
 done
 IFS=$OLDIFS
 
+REDIRECT_FROM_INTERFACE_IP="$(ifconfig ${REDIRECT_FROM_INTERFACE} | grep 'inet ' | awk '{ print $2 }')"
+
 echo "iptables -t nat -I PREROUTING 1 -i ${REDIRECT_FROM_INTERFACE} -p tcp -m tcp --dport ${REDIRECT_TO_PORT} -j ACCEPT"
 iptables -t nat -I PREROUTING 1 -i ${REDIRECT_FROM_INTERFACE} -p tcp -m tcp --dport ${REDIRECT_TO_PORT} -j ACCEPT
+
+echo "Redirect from interface ${REDIRECT_FROM_INTERFACE} public IP is {REDIRECT_FROM_INTERFACE_IP}"
 
 echo "Redirecting port ${REDIRECT_FROM_PORT} to ${REDIRECT_TO}:${REDIRECT_TO_PORT}"
 /usr/bin/redir -s -n ":${REDIRECT_FROM_PORT}" "${REDIRECT_TO}:${REDIRECT_TO_PORT}" &
