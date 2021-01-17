09f2510616aad48d838fb0f8445057574cb2c040
Matti Rantakömi <matti@rantakomi.fi>
2021-01-17 09:11:06 +0000

    korjattu typo

--- layers/entrypoint.sh
+++ layers/entrypoint.sh
@@ -23,7 +23,7 @@ REDIRECT_FROM_INTERFACE_IP="$(ifconfig ${REDIRECT_FROM_INTERFACE} | grep 'inet '
 echo "iptables -t nat -I PREROUTING 1 -i ${REDIRECT_FROM_INTERFACE} -p tcp -m tcp --dport ${REDIRECT_TO_PORT} -j ACCEPT"
 iptables -t nat -I PREROUTING 1 -i ${REDIRECT_FROM_INTERFACE} -p tcp -m tcp --dport ${REDIRECT_TO_PORT} -j ACCEPT
 
-echo "Redirect from interface ${REDIRECT_FROM_INTERFACE} public IP is {REDIRECT_FROM_INTERFACE_IP}"
+echo "Redirect from interface ${REDIRECT_FROM_INTERFACE} public IP is ${REDIRECT_FROM_INTERFACE_IP}"
 
 echo "Redirecting port ${REDIRECT_FROM_PORT} to ${REDIRECT_TO}:${REDIRECT_TO_PORT}"
 /usr/bin/redir -s -n ":${REDIRECT_FROM_PORT}" "${REDIRECT_TO}:${REDIRECT_TO_PORT}" &
